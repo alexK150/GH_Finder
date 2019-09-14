5704bbf2cabacceacc866a3ee1ee96baef56788b
Alex <akuzin20@gmail.com>
2019-09-14 17:56:54 +0300

    Init v2 +UserItem

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './bootstrap.styles.css';
 import Navbar from "./components/layout/Navbar";
+import UserItem from "./components/UserItem";
 
 class App extends React.Component{
     render(){
         return (
-            <nav className="navbar bg-dark">
-                <Navbar />
-            </nav>
+            <div>
+                <nav className="navbar bg-dark">
+                    <Navbar />
+                </nav>
+                <div>
+                    <UserItem/>
+                </div>
+            </div>
+
         );
     }
 
---
+++ src/components/UserItem.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+
+class UserItem extends Component {
+
+    state ={
+        id: 'id',
+        login: 'mojo',
+        avatarUrl: 'https://avatars1.githubusercontent.com/u/5550850?s=400&v=4',
+        htmlUrl: 'https://github.com/bradtraversy'
+    }
+
+    render() {
+        const {login, avatarUrl, htmlUrl} = this.state;
+        return (
+            <div className='card text-center'>
+                <img
+                    src={avatarUrl}
+                    alt=""
+                    className='round-img'
+                    style={{width: '60px'}}
+                />
+                <h3>{login}</h3>
+                <div>
+                    <a href={htmlUrl} className='btn btn-dark btn-sm my-1'>More
+                    </a>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default UserItem;
