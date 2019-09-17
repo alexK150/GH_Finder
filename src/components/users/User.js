b38c0aa09c33dadb94edbc4840a5c4221cf96a7c
Alex <akuzin20@gmail.com>
2019-09-17 15:32:16 +0300

    Init v5 +Spinner component

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,17 +1,26 @@
-import React, {Component} from 'react';
+import React from 'react';
 import UserItem from './UserItem'
+import Spinner from "../layout/Spinner.js";
+import PropTypes from 'prop-types'
 
-class User extends Component {
+const User=({users, isLoaded})=> {
 
-    render() {
+    if (isLoaded){
+       return <Spinner/>
+    }else {
         return (
             <div style={userStyle}>
-                {this.props.users.map(user => (
+                {users.map(user => (
                     <UserItem key={user.id} user={user}/>
                 ))}
             </div>
         );
     }
+}
+
+User.propTypes = {
+    users: PropTypes.array.isRequired,
+    isLoaded: PropTypes.bool.isRequired
 }
 
 const userStyle ={
