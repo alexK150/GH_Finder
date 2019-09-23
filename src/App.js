507b148b5f13f528cb7549fa3d8797a3b48d0efe
Alex <akuzin20@gmail.com>
2019-09-23 17:19:26 +0300

    Init v22 implementation of AlertContext

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import './App.css';
 import Navbar from "./components/layout/Navbar";
@@ -8,35 +8,29 @@ import Alert from './components/layout/Alert'
 import AboutInfo from "./components/pages/AboutInfo";
 import User from "./components/users/User";
 import GithubState from "./context/github/GithubState";
+import AlertState from "./context/alert/AlertState";
 
 const App = () => {
-    const [alert, setAlert] = useState(null);
 
-    const showAlert = (message, type) => {
-        setAlert({message, type});
-
-        setTimeout(() => setAlert(null), 3000)
-    }
 
     //Clear users from state
 
     return (
         <GithubState>
+            <AlertState>
             <Router>
                 <div className='App'>
                     <nav>
                         <Navbar/>
                     </nav>
                     <div className='container'>
-                        <Alert alert={alert}/>
+                        <Alert />
                         <Switch>
                             <Route exact
                                    path='/'
                                    render={props =>
                                        (<>
-                                           <Search
-                                               setAlert={showAlert}
-                                           />
+                                           <Search/>
                                            <Users />
                                        </>)
                                    }/>
@@ -51,6 +45,7 @@ const App = () => {
                     </div>
                 </div>
             </Router>
+            </AlertState>
         </GithubState>
     );
 }
